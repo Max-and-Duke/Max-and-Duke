65a2dec756406fc76aef4f500882375667f34667
AugustusZ <augustus.z@outlook.com>
2016-09-14 16:32:25 -0700

    create Unity project

---
+++ MAD/Assets/Scripts/Rotate.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	public float delay = 2.0f;
+	public bool active = true;
+
+	public int rotationDirection = -1; // -1 for clockwise, 1 for anti-clockwise
+	public int rotationStep = 10;    // should be less than 90
+
+	private Vector3 currentRotation, targetRotation;
+
+	void Start () {
+		StartCoroutine (objectRotator ());
+		rotateObject ();
+	}
+
+	IEnumerator objectRotator() {
+		yield return new WaitForSeconds (delay);
+
+		if (active) {
+			rotateObject ();
+		}
+			
+		StartCoroutine (objectRotator ());
+	}
+
+	void rotateObject()
+	{
+		currentRotation = gameObject.transform.eulerAngles;
+		targetRotation.z = (currentRotation.z + (90 * rotationDirection));
+		StartCoroutine (objectRotationAnimation());
+	}
+
+
+	IEnumerator objectRotationAnimation()
+	{
+		// add rotation step to current rotation.
+		currentRotation.z += (rotationStep * rotationDirection);
+		gameObject.transform.eulerAngles = currentRotation;
+
+		yield return new WaitForSeconds (0);
+
+		if (((int)currentRotation.z >
+			(int)targetRotation.z && rotationDirection < 0)  ||  // for clockwise
+			((int)currentRotation.z <  (int)targetRotation.z && rotationDirection > 0)) // for anti-clockwise
+		{
+			StartCoroutine (objectRotationAnimation());
+		}
+		else
+		{
+			gameObject.transform.eulerAngles = targetRotation;
+		}
+	}
+}
