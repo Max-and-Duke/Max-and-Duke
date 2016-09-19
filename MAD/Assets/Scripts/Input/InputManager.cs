d88178a60f2b515415de4c215ac034aec4bb3932
xyfighter <yxu695@usc.edu>
2016-09-18 22:03:37 -0700

    change ui buttons

--- MAD/Assets/Scripts/Input/InputManager.cs
+++ MAD/Assets/Scripts/Input/InputManager.cs
@@ -66,43 +66,75 @@ public class InputManager : MonoBehaviour {
 	
 	}
 
-	private void OnGUI(){
-
-		if (GUI.Button (new Rect (15, 15, 50, 50), "left")) {
-			if (curDog == Dog.Max) {
-				maxWalk.body2d.velocity = new Vector2 (-maxWalk.speed, maxWalk.body2d.velocity.y);
-			} else {
-				dukeWalk.body2d.velocity = new Vector2 (-dukeWalk.speed, dukeWalk.body2d.velocity.y);
-			}
-		
-		}
-
-		if (GUI.Button (new Rect (80, 15, 50, 50), "right")) {
-			if (curDog == Dog.Max) {
-				maxWalk.body2d.velocity = new Vector2 (maxWalk.speed, maxWalk.body2d.velocity.y);
-			} else {
-				dukeWalk.body2d.velocity = new Vector2 (dukeWalk.speed, dukeWalk.body2d.velocity.y);
-			}
-		}
-
-
-
-		if (GUI.Button (new Rect (900, 15, 50, 50), "jump")) {
-			if (curDog == Dog.Max)
-				maxJump.body2d.velocity = new Vector2 (maxJump.body2d.velocity.x, maxJump.jumpSpeed);
-			else {
-				dukeJump.body2d.velocity = new Vector2 (dukeJump.body2d.velocity.x, dukeJump.jumpSpeed);
-			}
-		}
-	
-		if (GUI.Button (new Rect (450, 15, 50, 50), "switch")) {
-			if (curDog == Dog.Max) {
-				curDog = Dog.Duke;
-			} else {
-				curDog = Dog.Max;
-			}
+	public void moveLeft(){
+		if (curDog == Dog.Max) {
+			maxWalk.body2d.velocity = new Vector2 (-maxWalk.speed, maxWalk.body2d.velocity.y);
+		} else {
+			dukeWalk.body2d.velocity = new Vector2 (-dukeWalk.speed, dukeWalk.body2d.velocity.y);
 		}
 	}
+
+	public void moveRight(){
+		if (curDog == Dog.Max) {
+			maxWalk.body2d.velocity = new Vector2 (maxWalk.speed, maxWalk.body2d.velocity.y);
+		} else {
+			dukeWalk.body2d.velocity = new Vector2 (dukeWalk.speed, dukeWalk.body2d.velocity.y);
+		}
+	}
+
+
+	public void jump(){
+		if (curDog == Dog.Max)
+			maxJump.body2d.velocity = new Vector2 (maxJump.body2d.velocity.x, maxJump.jumpSpeed);
+		else {
+			dukeJump.body2d.velocity = new Vector2 (dukeJump.body2d.velocity.x, dukeJump.jumpSpeed);
+		}
+	}
+
+	public void switchDog(){
+		if (curDog == Dog.Max) {
+			curDog = Dog.Duke;
+		} else {
+			curDog = Dog.Max;
+		}
+	}
+//	private void OnGUI(){
+//
+//		if (GUI.Button (new Rect (15, 15, 50, 50), "left")) {
+//			if (curDog == Dog.Max) {
+//				maxWalk.body2d.velocity = new Vector2 (-maxWalk.speed, maxWalk.body2d.velocity.y);
+//			} else {
+//				dukeWalk.body2d.velocity = new Vector2 (-dukeWalk.speed, dukeWalk.body2d.velocity.y);
+//			}
+//		
+//		}
+//
+//		if (GUI.Button (new Rect (80, 15, 50, 50), "right")) {
+//			if (curDog == Dog.Max) {
+//				maxWalk.body2d.velocity = new Vector2 (maxWalk.speed, maxWalk.body2d.velocity.y);
+//			} else {
+//				dukeWalk.body2d.velocity = new Vector2 (dukeWalk.speed, dukeWalk.body2d.velocity.y);
+//			}
+//		}
+//
+//
+//
+//		if (GUI.Button (new Rect (900, 15, 50, 50), "jump")) {
+//			if (curDog == Dog.Max)
+//				maxJump.body2d.velocity = new Vector2 (maxJump.body2d.velocity.x, maxJump.jumpSpeed);
+//			else {
+//				dukeJump.body2d.velocity = new Vector2 (dukeJump.body2d.velocity.x, dukeJump.jumpSpeed);
+//			}
+//		}
+//	
+//		if (GUI.Button (new Rect (450, 15, 50, 50), "switch")) {
+//			if (curDog == Dog.Max) {
+//				curDog = Dog.Duke;
+//			} else {
+//				curDog = Dog.Max;
+//			}
+//		}
+//	}
 	
 	// Update is called once per frame
 	void Update () {
