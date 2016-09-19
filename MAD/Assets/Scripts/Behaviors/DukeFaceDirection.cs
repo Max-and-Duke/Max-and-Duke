1f112dc071444f907aff471b45f073a13fe480e5
xyfighter <yxu695@usc.edu>
2016-09-18 22:29:16 -0700

    change max scale to be smaller

--- MAD/Assets/Scripts/Behaviors/DukeFaceDirection.cs
+++ MAD/Assets/Scripts/Behaviors/DukeFaceDirection.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FaceDirection : AbstractBehavior {
+public class MaxFaceDirection : AbstractBehavior {
 
 	// Use this for initialization
 	void Start () {
