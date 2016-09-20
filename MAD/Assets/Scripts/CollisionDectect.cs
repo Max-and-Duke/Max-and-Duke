be2c8a36bdcf317df7dccbebc63c37f5c734db92
JBWKZsf <zhonghaow93@gmail.com>
2016-09-20 13:30:16 -0700

    drag and change image; count tool number

--- MAD/Assets/Scripts/CollisionDectect.cs
+++ MAD/Assets/Scripts/CollisionDectect.cs
@@ -5,7 +5,7 @@ public class CollisionDectect : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D collider) {
 		Debug.Log ("Enter~");
-		if (collider.gameObject.name == "Board") {
+		if (collider.gameObject.name == "Draggable-board1") {
 			Debug.Log ("board zhuang");
 		}
 
--- MAD/Assets/Scripts/Draggable.cs
+++ MAD/Assets/Scripts/Draggable.cs
@@ -5,63 +5,105 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
-	public static GameObject itemBeingDragged;
-	private Vector2 startPosition;
-	private Vector2 inputPosition;
-	private Vector2 touchOffset;
-	private Vector2 pivotOffset;
-	private Transform startParent;
+	public GameObject itemBeingDragged;
+	public Sprite itemOutSidePanel;
 	public Text countTool;
 	public GameObject panel;
-	private bool collide = false;
+	public bool collide = false;
+
+	private static int boardNum = 2;
+	private static int nailNum = 4;
+	private float originImageWidth;
+	private float originImageHeight;
+
+	private float boardWidth = 500f;
+	private float boardHeight = 60f;
+	private float nailWidth = 50f;
+	private float nailHeight = 50f;
+	private BoxCollider2D sceneCollider;
+
+	private Vector2 startPosition;
+	private Vector2 inputPosition;
+	private Vector2 pivotOffset;
+	private Transform startParent;
+	private Vector2 oneClickStartPosition;
+	private Image image;
+	private Sprite originImageSprite;
+
+	void Start(){
+		image = itemBeingDragged.GetComponent<Image>();
+		originImageWidth = image.rectTransform.rect.width;
+		originImageHeight = image.rectTransform.rect.height;
+		//print (originImage.rectTransform.rect.height);
+		originImageSprite = image.sprite;
+		startPosition = itemBeingDragged.transform.position;
+		if (itemBeingDragged.tag == "Board") {
+			countTool.text = "x " + boardNum;
+		} else if (itemBeingDragged.tag == "Nail"){
+			countTool.text = "x " + nailNum;
+		}
+	}
+
 
 	public void OnBeginDrag(PointerEventData eventData) {
 		itemBeingDragged = gameObject;
-		startPosition = transform.position;
+		oneClickStartPosition = itemBeingDragged.transform.position;
 		inputPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		pivotOffset = inputPosition - startPosition;
-		//startParent = transform.parent;
-		Debug.Log (startPosition);
-		//Debug.Log ("OnBeginDrag");
+		pivotOffset = inputPosition - oneClickStartPosition;
+		Debug.Log ("onbegin()");
+		if (itemBeingDragged.GetComponent<Collider2D> ().bounds.Intersects (panel.GetComponent<Collider2D> ().bounds)) {
+			if (itemBeingDragged.tag == "Board") {
+				boardNum = boardNum - 1;
+				countTool.text = "x " + boardNum;
+				image.rectTransform.sizeDelta = new Vector2 (boardWidth, boardHeight);
+
+				sceneCollider = itemBeingDragged.GetComponent<BoxCollider2D>();
+				sceneCollider.size = new Vector2 (boardWidth, boardHeight);
+		
+//				sceneCollider.bounds.size = new Vector3 (boardWidth, boardHeight, 0);
+			} else if (itemBeingDragged.tag == "Nail"){
+				nailNum = nailNum - 1;
+				countTool.text = "x " + nailNum;
+				image.rectTransform.sizeDelta = new Vector2 (nailWidth, nailHeight);
+			}
+
+			image.sprite = itemOutSidePanel;
+		}
+
 	}
 
 	void OnTriggerEnter2D(Collider2D collider) {
-		//		if (collider.gameObject.name == "Board") {
-		Debug.Log ("zhuang");
-		//		}
+		if (collider.gameObject.name == "ToolBarPanel") {
+			collide = true;
+		}
 
 	}
 
 	public void OnDrag(PointerEventData eventData) {
-		//Debug.Log ("OnDrag");
+		
 		inputPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-//		RaycastHit2D[] touches = Physics2D.RaycastAll(inputPosition, inputPosition, 1f);
-//		if (touches.Length > 0)
-//		{
-//			var hit = touches[0];
-//			if (hit.transform != null)
-//			{
-//				
-////				itemBeingDragged = hit.transform.gameObject;
-//				touchOffset = (Vector2)hit.transform.position - inputPosition;
-//				//                    draggedObject.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
-//			}
-//		}
 
 		transform.position = inputPosition - pivotOffset;
-//		itemBeingDragged.transform.position = inputPosition;
 
-//		this.transform.position = new Vector3(Input.mousePosition.x - Screen.width / 2  * PixelPerfectCamera.pixelToUnits, Input.mousePosition.y - Screen.height / 2 * PixelPerfectCamera.pixelToUnits, 0);
 	}
 		
 
 	public void OnEndDrag(PointerEventData eventData) {
-		itemBeingDragged = null;
+		
 
-//		if (transform.renderer.bounds.Intersects(object2.renderer.bounds)) {
-//			// Do some stuff
-//		}
-		//Debug.Log ("OnEndDrag");
+		if (itemBeingDragged.GetComponent<Collider2D>().bounds.Intersects (panel.GetComponent<Collider2D>().bounds)) {
+			if (itemBeingDragged.tag == "Board") {
+				boardNum = boardNum + 1;
+				countTool.text = "x " + boardNum;
+			} else if (itemBeingDragged.tag == "Nail"){
+				nailNum = nailNum + 1;
+				countTool.text = "x " + nailNum;
+			}
+			image.sprite = originImageSprite;
+
+			image.rectTransform.sizeDelta = new Vector2 (originImageWidth, originImageHeight);
+			transform.position = startPosition;
+		}
 	}
 
 
