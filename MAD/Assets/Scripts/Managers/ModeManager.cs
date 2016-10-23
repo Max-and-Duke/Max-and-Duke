fa863ea0884ce13bfb67831b7469212b96cfdaa6
AugustusZ <augustus.z@outlook.com>
2016-10-23 00:59:01 -0700

    Resolve #52 , resolve #51 , add sprite-sheets for Max
    Kinda messed up Max prefab rendering :( @xyfighter

--- MAD/Assets/Scripts/Managers/ModeManager.cs
+++ MAD/Assets/Scripts/Managers/ModeManager.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
 public enum Mode {
+	// must be indexed for `componentSettings`
 	Deploy = 0,
 	Play = 1
 }
@@ -11,62 +13,121 @@ public enum Mode {
 public class ModeManager : MonoBehaviour {
 	public Sprite[] modeSprite;
 
-	private Mode mode = Mode.Deploy;
+	private Mode currentMode = Mode.Deploy;
 	private int numOfModes = System.Enum.GetNames (typeof(Mode)).Length;
-	private Dictionary<string, GameObject> gameObjectPool = new Dictionary<string, GameObject>();
-	private Dictionary<string, bool> gameObjectSettingsForDeployMode = new Dictionary<string, bool>
-	{
-		{"Console", false},
-		{"Toolbox", true}
-	}; // will be stored outside code.s
-	private Dictionary<string, bool> gameObjectSettingsForPlayMode = new Dictionary<string, bool>
-	{
-		{"Console", true},
-		{"Toolbox", false}
-	}; // will be stored outside code.s
-		
 
-	void SetGameObjectActiveInPool(string target, bool active) {
-		GameObject targetGameObject;
+	// =====================================================================================
+	// this holds all components effected by mode-changing, AKA "Toolbox" and "Console" for now
+	private Dictionary<string, GameObject> componentsPool = new Dictionary<string, GameObject>();
+	private Dictionary<string, float[]> componentSettings;
+	// =====================================================================================
 
-		if (gameObjectPool.ContainsKey (target)) {
-			targetGameObject = gameObjectPool [target];
-		} else {
-			targetGameObject = GameObject.Find (target);
-			gameObjectPool.Add (target, targetGameObject);
-		}
-
-		targetGameObject.SetActive (active);
+	private void SetPosY (GameObject gameObject, float posY) {
+		var vector = gameObject.transform.position;
+		vector[1] = posY;
+		gameObject.transform.position = vector;
 	}
 
-	void SetAllGameObjectsActiveWithSettings (Dictionary<string, bool> gameObjectSettingsForDeployMode) {
-		foreach (KeyValuePair<string, bool> entry in gameObjectSettingsForDeployMode) {
-			SetGameObjectActiveInPool (entry.Key, entry.Value);
+	private void LoadSettingsFromFile (string fileName) {
+		/// for loading json from outside file, 
+		/// see http://pressonegames.com/parsing-json-files-in-unity/
+		TextAsset asset = Resources.Load(fileName) as TextAsset; // which is never used.
+
+		/// DATA STRUCTURE EXPLANATION:
+		/// for each entry in the dictionary, 
+		/// - KEY: gameObject name (string)
+		/// - VALUE: postions for each Mode using the Mode index as array index
+		/// E.g. `Mode.Deploy` is indexed as `0`, so the postion (in Y)
+		/// of gameObject `Console` in Deploy mode is 
+		/// 	float posY = componentSettings["Console"][0];
+		/// the last number in the array is the idle position (off-screen position)
+		componentSettings = new Dictionary<string, float[]> 
+		{
+			{ "Console", new []{ -1080.0f, -540.0f, -1080.0f } },
+			{ "Toolbox", new []{ -440.0f, -640.0f, -640.0f } },
+		};
+	}
+
+	/// <summary>
+	/// This method:
+	/// 1. reads comopent position configuration from resourse file and put it in `componentSettings`
+	/// 2. intializes all components' postion is scene
+	/// 3. puts all components' in `componentsPool`
+	/// </summary>
+	private void LoadComponents() {
+		LoadSettingsFromFile ("ModeSettings");
+
+		foreach (KeyValuePair<string, float[]> entry in componentSettings) {
+			var componentName = entry.Key;
+			var component = GameObject.Find (componentName);
+			SetPosY (component, entry.Value[0]);
+			componentsPool.Add (componentName, component);
 		}
+	}
+
+	/// <summary>
+	/// Slides `gameObject` to `positionSlideTo` with continuous translation.
+	/// </summary>
+	private void SlideTo (GameObject gameObject, float positionSlideTo) {
+		// this is just a dummy implementation for now
+		var vector = gameObject.transform.position;
+		vector [1] = positionSlideTo;
+		iTween.MoveTo(gameObject, vector, 1.0f);
 	}
 
 	public void GoToMode(Mode mode) {
-		/// sprite for button image sprite should be update as well... (TBImplement)
-
-		switch (mode) {
-		case Mode.Deploy:
-			SetAllGameObjectsActiveWithSettings (gameObjectSettingsForDeployMode);
-			setIsKinematic (true);
-			break;
-		case Mode.Play:
-			SetAllGameObjectsActiveWithSettings (gameObjectSettingsForPlayMode);
-			break;
+		currentMode = mode;
+		foreach (KeyValuePair<string, float[]> entry in componentSettings) {
+			SlideTo (componentsPool[entry.Key], entry.Value[(int)mode]);
 		}
 	}
-		
+
 	void Awake() {
-		GoToMode (Mode.Deploy);
+		LoadComponents ();
+	}
+		
+	public void HideAllComponents() {
+		foreach (KeyValuePair<string, float[]> entry in componentSettings) {
+			var component = componentsPool [entry.Key];
+			var posY = entry.Value [entry.Value.Length - 1]; // this is the idle position
+			SetPosY(component, posY);
+		}
 	}
 
-	void ToggleAllGameObjectsActive () {
-		foreach(KeyValuePair<string, GameObject> entry in gameObjectPool)
-		{
+	/// <summary>
+	/// Returns the next mode based on the enumeration order of Mode.
+	/// </summary>
+	private Mode GetNextMode() {
+		return (Mode)((1 + (int)currentMode) % numOfModes); 
+	}
 
+	private void SwithButtonImage() {
+		GameObject modeButton = GameObject.Find ("Mode Button");
+		Image image = modeButton.GetComponent<Image> ();
+		image.sprite = modeSprite [(int) currentMode];
+	}
+
+	public void SwitchMode() {
+		currentMode = GetNextMode();
+		GoToMode (currentMode);
+		SwithButtonImage ();
+	}
+
+	// ================================================================
+	// ================================================================
+
+	/// OBSOLETE
+	void ToggleAllGameObjectsActive () {
+		foreach(KeyValuePair<string, GameObject> entry in componentsPool)
+		{
+			//////////////////////////////////////////////////////////
+			// Hey Zhonghao, 
+			// FYI, 
+			// gameObjects (Toolbox and Console) are ALWAYS active now.
+			// you can use `currentMode` to get current mode. and you
+			// really wanna do what you wanna do here somewhere else
+			// cuz this method is OBSOLETE now.
+			//////////////////////////////////////////////////////////
 
 			bool active = !entry.Value.activeInHierarchy;
 
@@ -91,27 +152,7 @@ public class ModeManager : MonoBehaviour {
 
 				}
 			}
-
-//			Debug.Log ("Just toggled <" + entry.Key + "> to " + active.ToString () + ".");
 		}
-	}
-
-	public void DisableAllGameObjectsInPool () {
-		foreach(KeyValuePair<string, GameObject> entry in gameObjectPool)
-		{
-			entry.Value.gameObject.SetActive (false);
-		}
-	}
-		
-	public void SwitchMode() {
-		ToggleAllGameObjectsActive ();
-
-		GameObject modeButton = GameObject.Find ("Mode Button");
-		Image image = modeButton.GetComponent<Image> ();
-
-		mode = (Mode)((1 + (int)mode) % numOfModes); // switch to next mode
-
-		image.sprite = modeSprite [(int) mode];
 	}
 
 	public void setIsKinematic(bool a){
