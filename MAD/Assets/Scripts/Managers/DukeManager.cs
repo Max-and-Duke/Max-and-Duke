08c9806fd9f0279dbcf2e7c5a9a1e598cc476415
xyfighter <yxu695@usc.edu>
2016-11-02 00:56:04 -0700

    add animations

--- MAD/Assets/Scripts/Managers/DukeManager.cs
+++ MAD/Assets/Scripts/Managers/DukeManager.cs
@@ -1,19 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MaxManager : MonoBehaviour {
-
-	private Rigidbody2D maxBody;
+public class DukeManager : MonoBehaviour {
+	private Rigidbody2D dukeBody;
 	private Animator animator;
 	// Use this for initialization
 	void Start () {
-		maxBody = GetComponent<Rigidbody2D> ();
+		dukeBody = GetComponent<Rigidbody2D> ();
 		animator = GetComponent<Animator> ();
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-		float absX = Mathf.Abs (maxBody.velocity.x);
+		float absX = Mathf.Abs (dukeBody.velocity.x);
 		if(absX == 0){
 			animator.SetInteger ("AnimState", 0);
 		}
@@ -21,11 +20,6 @@ public class MaxManager : MonoBehaviour {
 		if (absX > 0) {
 			animator.SetInteger ("AnimState", 1);
 		}
-
-//		var z = Mathf.Clamp (transform.rotation.z + 1, -60, 60);
-//
-//		transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, z);
-			
-			
+	
 	}
 }
